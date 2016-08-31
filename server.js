8e18ab18d52f0a58e56736dda484bdc24e04ecde
Douglas Marra <douglasmsi@gmail.com>
2016-08-31 16:16:00 -0300

    ...

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var restify = require('restify');
 var host = process.env.HOST || '127.0.0.1';
 var port = process.env.PORT || '8080';
 
+
 var app = restify.createServer({
   name: 'Coffee TVShow Server'
 });
+
+
 
 app.use(restify.queryParser());
 app.use(restify.bodyParser());
@@ -14,11 +17,14 @@ app.use(function logger(req,res,next) {
   next();
 });
 
+
+
 app.on('uncaughtException',function(request, response, route, error){
+  console.log(new Date(),request.method,request.url);
   console.error(error.stack);
   response.send(error);
 });
 
 app.listen(port,host, function() {
-  console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s', app.name, app.url);
 });
