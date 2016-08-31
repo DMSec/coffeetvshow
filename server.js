5887504458574818a9230763a4059820174b47bb
Douglas Marra <douglasmsi@gmail.com>
2016-08-31 14:11:24 -0300

    Create the server with restify

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-var app = require('./app/config/custom-express')();
+var restify = require('restify');
+var host = process.env.HOST || '127.0.0.1';
+var port = process.env.PORT || '8080';
 
-app.listen(3000,function(){
-  console.log("Server started!");
+var server = restify.createServer({
+  name: 'Things API Server'
+});
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.use(function logger(req,res,next) {
+  console.log(new Date(),req.method,req.url);
+  next();
+});
+
+server.on('uncaughtException',function(request, response, route, error){
+  console.error(error.stack);
+  response.send(error);
+});
+
+server.listen(port,host, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
