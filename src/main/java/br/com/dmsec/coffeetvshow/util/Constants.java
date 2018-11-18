c7fd8f47e81252ef02cbd23c2479b1448f43a827
Douglas Marra <douglasmsi@gmail.com>
2018-11-18 21:46:35 -0200

    Update Constants.java

--- src/main/java/br/com/dmsec/coffeetvshow/util/Constants.java
+++ src/main/java/br/com/dmsec/coffeetvshow/util/Constants.java
@@ -8,9 +8,9 @@
 package br.com.dmsec.coffeetvshow.util;
 
 public class Constants {
-    public static final String TVST_CLIENT_ID = "-COGBbPt1-gM76uWrYLc";
-    public static final String TVST_CLIENT_SECRET = "xThbKVEe_cJSxLGBS4k0Wz3zjrC987cwqV4edsAQ";
-    public static final String TVST_USER_AGENT = "tvshowtime-plex";
+    public static final String TVST_CLIENT_ID = "PUT YOUR CLIENT_ID here. You need request to tvshowtime team";
+    public static final String TVST_CLIENT_SECRET = "PUT YOUR CLIENT_ID here. You need request to tvshowtime team";
+    public static final String TVST_USER_AGENT = "coffeetvshow";
     public static final String TVST_RATE_REMAINING_HEADER = "X-RateLimit-Remaining";
     public static final String TVST_AUTHORIZE_URI = "https://api.tvshowtime.com/v1/oauth/device/code";
     public static final String TVST_ACCESS_TOKEN_URI = "https://api.tvshowtime.com/v1/oauth/access_token";
@@ -19,7 +19,7 @@ public class Constants {
     public static final String TVST_SHOW = "https://api.tvtime.com/v1/show";
     public static final String TVST_LIBRARY = "https://api.tvtime.com/v1/library";
     public static final String TVST_USER = "https://api.tvtime.com/v1/user";
-    public static final String OMDB_KEY ="297b27ff";
+    public static final String OMDB_KEY ="YOU NEED GENERATE THE KEY from omdb api";
     public static final String OMDB_URI = "http://www.omdbapi.com/?apikey="+OMDB_KEY+"&";
     public static final String EZTV_URI = "https://eztv.ag/api/get-torrents?imdb_id=";
 
